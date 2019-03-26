e863d2ddb262acd289a205084c2365be067574b3
Kevin Chen <xkevinchen@gmail.com>
2019-03-26 16:42:27 -0400

    Deployed app with Now

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4914726f1487323939de";
+/******/ 	var hotCurrentHash = "cb5610bf2d75dfd06a8d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,55 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+
+function getPosts() {
+  return [
+    { id: 'thank-you-next', title: ' thank you, next.js' },
+    { id: 'learn-next', title: 'learn next.js' },
+  ]
+}
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+    li {
+      list-style: none;
+      margin: 5px 0;
+    }
+    
+    a {
+      text-decoration: none;
+      color: blue;
+    }
+    
+    a:hover {
+      opacity: 0.6;
+    }
+  `}</style>
+  </li>
 )
 
-export default Index
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Awesome Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+        
+        ul {
+          padding: 0;
+        }
+      `}</style>
+    </Layout>
+  )
+}
